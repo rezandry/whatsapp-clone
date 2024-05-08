7944ee3303bc976fa32a519f106332ddf161e3f3
rezandry <andriyunantoreza@gmail.com>
2024-05-08 22:45:54 +0700

    TASK-1 add base project code

---
+++ src/main/java/rezandry/wacloneapi/WhatsappCloneApiApplication.java
@@ -0,0 +1,13 @@
+package rezandry.wacloneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappCloneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rezandry/wacloneapi/WhatsappCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rezandry.wacloneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
