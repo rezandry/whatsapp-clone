77f9e3aa4a98e7d1c597fe5725864a5096357183
rezandry <andriyunantoreza@gmail.com>
2024-05-10 01:43:39 +0700

    Task 4 add test register api

---
+++ src/test/java/rezandry/wacloneapi/UserApiTest.java
@@ -0,0 +1,92 @@
+package rezandry.wacloneapi;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import rezandry.wacloneapi.entity.User;
+import rezandry.wacloneapi.model.RegisterUserRequest;
+import rezandry.wacloneapi.model.WebResponse;
+import rezandry.wacloneapi.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserApiTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void testRegisterSuccess() throws Exception {
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setName("Reza");
+        request.setPassword("secret");
+        request.setPhoneNumber("085234123123");
+
+        mockMvc.perform(
+                post("/api/v1/user/register")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(
+                        status().isOk())
+                .andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+                            });
+
+                    assertEquals("OK", response.getData());
+                });
+    }
+
+    @Test
+    void testRegisterDuplicated() throws Exception {
+
+        User user = new User();
+        user.setName("Reza");
+        user.setPassword("rahasia");
+        user.setPhoneNumber("085234123123");
+        userRepository.save(user);
+
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setName("Reza");
+        request.setPassword("secret");
+        request.setPhoneNumber("085234123123");
+
+        mockMvc.perform(
+                post("/api/v1/user/register")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(
+                        status().isBadRequest())
+                .andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+                            });
+
+                    assertEquals("Phone number already registered", response.getErrors());
+                });
+    }
+}
